c8335d3ac28c39bb48c3f8f19a6ca00301b5bb8c
Shalitha Lakruwan <69634572+shalithacode@users.noreply.github.com>
2023-06-15 23:01:09 +0530

    Implement form component

--- src/Components/Meals/MealItem/MealItemForm.js
+++ src/Components/Meals/MealItem/MealItemForm.js
@@ -1,7 +1,23 @@
-import React from "react";
+import Input from "../../UI/Input";
+import classes from "./MealItemForm.module.css";
 
-function MealItemForm() {
-  return <div></div>;
-}
+const MealItemForm = (props) => {
+  return (
+    <form className={classes.form}>
+      <Input
+        label="Amount"
+        input={{
+          id: "amount_" + props.id,
+          type: "number",
+          min: "1",
+          max: "5",
+          step: "1",
+          defaultValue: "1",
+        }}
+      />
+      <button>+ Add</button>
+    </form>
+  );
+};
 
 export default MealItemForm;
