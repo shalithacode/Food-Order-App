a48e32581c498db1164d4b46e03ea77b63070138
Shalitha Lakruwan <69634572+shalithacode@users.noreply.github.com>
2023-06-16 23:56:35 +0530

    Addind visual effect for add cart button.

--- src/Components/Layout/HeaderCartButton.js
+++ src/Components/Layout/HeaderCartButton.js
@@ -1,16 +1,29 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import cartContext from "../../store/cart-context";
 import classes from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
 function HeaderCartButton(props) {
+  const [buttonIsHighlighted, setButtonIsHighlighted] = useState(false);
+
   const cartCtx = useContext(cartContext);
 
   const numberOffCartItems = cartCtx.items.reduce(
     (currentNumber, item) => currentNumber + item.amount,
     0
   );
+
+  const btnClasses = `${classes.button} ${buttonIsHighlighted && classes.bump}`;
+  useEffect(() => {
+    if (cartCtx.items.length === 0) {
+      return;
+    }
+    setButtonIsHighlighted(true);
+    const timer = setTimeout(() => setButtonIsHighlighted(false), 300);
+
+    return clearTimeout(timer);
+  }, [cartCtx.items]);
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
